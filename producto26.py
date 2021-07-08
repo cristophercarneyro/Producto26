334e645998ccb8701fcb42b825a4036b320c66f2
cristophercarneyro <86906534+cristophercarneyro@users.noreply.github.com>
2021-07-07 23:18:07 -0400

    Simplicacion del código
    Quitamos print y lo pusimos dentros de ciclos y def
    
    Co-Authored-By: grominacv <87104482+grominacv@users.noreply.github.com>

--- producto26.py
+++ producto26.py
@@ -1,5 +1,17 @@
 import matplotlib.pyplot as plt
 
+
+def mostrarregiones():
+  print("\n")
+  print("      Elige una región: ")
+  j = 0
+  for i in range(15):
+    j = j + 1 
+    print(j,end='.')
+    print(" Región",lista[i])
+  print("17. Salir")
+  print("\n")
+
 def pedirRespGraf():
   print("\n")
   print ("1. Mostrar un gráfico de contagiados con síntomas NO acumulativos")
@@ -10,7 +22,7 @@ def grafico():
   correcto = False
   while (not correcto):
       try:
-          grafico = int(input("ingrese el grafico a elección: "))
+          grafico = int(input("Ingrese el grafico a elección: "))
           correcto = True
       except ValueError:
           print('Error, introduce un grafico valido')
@@ -29,104 +41,71 @@ def pedirNumeroEntero():
             correcto = True
         except ValueError:
             print('Error, introduce una región valida')
-
+    if not num == 17:
+      print("Elegiste: Región",lista[num-1])
     return num
-
+    
 salir = False
 opcion = 0
 
 while not salir:
-    print("\n")
-    print("1.  Arica y parinacota ")
-    print("2.  Tarapacá ")
-    print("3.  Antofagasta ")
-    print("4.  Atacama ")
-    print("5.  Coquimbo ")
-    print("6.  Valparaíso  ")
-    print("7.  Metropolitana de Santiago")
-    print("8.  Región del Libertador Gral.Bernardo O’Higgins")
-    print("9.  Región del Maule")
-    print("10. Región del Ñuble.")
-    print("11. Región del BioBio")
-    print("12. Región de la Araucanía")
-    print("13. Región de Los Ríos")
-    print("14. Rgión de los Lagos ")
-    print("15. Región Aisén del Gral.Carlos Ibáñez del Campo")
-    print("16. Región de Magallanes y la Antártica Chilena	")
-    print("17. Salir")
-    print("\n")
-    print("       Elige una opcion")
-
+    
+    mostrarregiones()
     opcion = pedirNumeroEntero()
-    print("Elegiste:",end=' ')
+
     if opcion == 1:
-        print("Arica y parinacota ")
         pedirRespGraf()  
         grafico()
         
-        
     elif opcion == 2:
-        print("Tarapacá")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 3:
-        print("Antofagasta")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 4:
-        print("Atacama")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 5:
-        print("Coquimbo")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 6:
-        print("Valparaíso")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 7:
-        print("Metropolitana de Santiago")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 8:
-        print("Región del Libertador Gral.Bernardo O’Higgins")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 9:
-        print("hola")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 10:
-        print("hola")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 11:
-        print("hola")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 12:
-        print("Región de la Araucanía")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 13:
-        print("Región de Los Ríos")
         pedirRespGraf()  
         grafico()
         
     elif opcion == 14:
-        print("Región de los Lagos")
         pedirRespGraf()  
         grafico()
         
@@ -135,7 +114,6 @@ while not salir:
         grafico()
         
     elif opcion == 16:
-        print("Región de Magallanes y la Antártica Chilena")
         pedirRespGraf()  
         grafico()
         
@@ -143,6 +121,6 @@ while not salir:
         salir = True
 
     else:
-        print("Introduce un numero entre 1 y 16")
+        print("Introduce un numero entre 1 y 17")
 
 print("Fin")
